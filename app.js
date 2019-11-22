1b78213c796252c00cfcf45bd79c01591f8c492f
= <jacex17@gmail.com>
2019-11-22 09:58:50 -0500

    new deployment

--- app.js
+++ app.js
@@ -41,19 +41,19 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("/*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//   });
+// }
 
-else {
-  app.use(express.static(path.join(__dirname, '/client/public')));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
-}
+// else {
+//   app.use(express.static(path.join(__dirname, '/client/public')));
+//   app.get("/*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/public/index.html"));
+//   });
+// }
 
 //SESSION
 app.use(session({
