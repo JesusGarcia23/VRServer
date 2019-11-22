40a912142a5bb4295392dc83926831f46f30c632
= <jacex17@gmail.com>
2019-11-22 10:44:47 -0500

    update heroku

--- configs/database/db.setup.js
+++ configs/database/db.setup.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true})
-mongoose.connect(process.env.MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+mongoose.connect('mongodb://heroku_4dbgpb04:5gr5b9tela03oi0d5vkm75pd95@ds031657.mlab.com:31657/heroku_4dbgpb04', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
